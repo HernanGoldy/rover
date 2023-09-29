03fe7728e85d4bbf8930f4a59ee2186fcbb3022b
HernanGoldy <hernanmdq@gmail.com>
2023-09-29 16:35:36 -0300

    creo proyecto Rover

---
+++ src/test/java/academy/atl/RoverApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.atl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

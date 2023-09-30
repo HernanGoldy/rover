ede28495a858feb89348bd12ea000e214b521de1
HernanGoldy <hernanmdq@gmail.com>
2023-09-30 19:01:47 -0300

    creo endpoint para crear la lista de comandos para el Rover

--- src/main/java/academy/atl/controllers/RoverController.java
+++ src/main/java/academy/atl/controllers/RoverController.java
@@ -1,5 +1,6 @@
 package academy.atl.controllers;
 
+import academy.atl.dto.ComandosDto;
 import academy.atl.dto.RoverDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,5 +13,11 @@ public class RoverController {
     @PostMapping("api/rover/")
     public void create(@RequestBody RoverDto rover) {
         System.out.println(rover);
+    }
+
+    // Para crear una lista de comandos para que los ejecute el Rover
+    @PostMapping("api/rover/comandos")
+    public void create(@RequestBody ComandosDto comandos) {
+        System.out.println(comandos);
     }
 }
---
+++ src/main/java/academy/atl/dto/ComandosDto.java
@@ -0,0 +1,10 @@
+package academy.atl.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ComandosDto {
+    private List<String> comandos;
+}

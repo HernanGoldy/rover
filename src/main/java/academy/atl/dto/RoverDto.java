5d064cbb29b0bfac9a0ee54c572af9ac11b8e19c
HernanGoldy <hernanmdq@gmail.com>
2023-09-30 09:51:22 -0300

    agrego a RoverDto @Data de Lombok

--- src/main/java/academy/atl/dto/RoverDto.java
+++ src/main/java/academy/atl/dto/RoverDto.java
@@ -1,12 +1,15 @@
 package academy.atl.dto;
 
 import academy.atl.models.Orientacion;
+import lombok.Data;
 
 /**
  * @apiNote Con la clase RoverDto me inventaré un objeto que contenga la mínima información
  * necesaria para ser utilizado por un endpoint.
  * Esto permite la creación de endpoints robustos.
  */
+
+@Data
 public class RoverDto {
     private Integer x;
     private Integer y;

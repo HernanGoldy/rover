f5872ecd04e1631e0cf731fdf952ada2b8fb5803
HernanGoldy <hernanmdq@gmail.com>
2023-09-30 10:07:24 -0300

    modifico comentario

--- src/main/java/academy/atl/dto/RoverDto.java
+++ src/main/java/academy/atl/dto/RoverDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 /**
  * @apiNote Con la clase RoverDto me inventaré un objeto que contenga la mínima información
  * necesaria para ser utilizado por un endpoint.
- * Esto permite la creación de endpoints robustos.
+ * Esta práctica permite la creación de endpoints robustos.
  */
 
 @Data

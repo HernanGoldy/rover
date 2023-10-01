04c17980fbe4ee638f35f5dd60c5bd6ab60c7dae
HernanGoldy <hernanmdq@gmail.com>
2023-10-01 08:49:52 -0300

    creo servicio para que el modelo se comunique con el controlador del obstáculo

--- src/main/java/academy/atl/controllers/ObstaculoController.java
+++ src/main/java/academy/atl/controllers/ObstaculoController.java
@@ -1,16 +1,32 @@
 package academy.atl.controllers;
 
 import academy.atl.dto.ObstaculoDto;
+import academy.atl.models.Obstaculo;
+import academy.atl.services.ObstaculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ObstaculoController {
+
+    @Autowired
+    private ObstaculoService service;
 
     // Para crear un obstáculo
     @PostMapping("api/obstaculo/")
     public void crear(@RequestBody ObstaculoDto obstaculo) {
+
         System.out.println(obstaculo);
+    }
+
+    // Para mostrar todos los obstáculos
+    @GetMapping("api/obstaculo/")
+    public List<Obstaculo> mostrarObstaculos() {
+        return service.mostrarObstaculos();
     }
 }
---
+++ src/main/java/academy/atl/services/ObstaculoService.java
@@ -0,0 +1,10 @@
+package academy.atl.services;
+
+import academy.atl.dto.ObstaculoDto;
+import academy.atl.models.Obstaculo;
+
+import java.util.List;
+
+public interface ObstaculoService {
+    public List<Obstaculo> mostrarObstaculos();
+}

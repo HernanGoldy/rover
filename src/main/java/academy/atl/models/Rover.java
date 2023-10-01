838e1423165860d7aa96672007ebe0855498b11d
HernanGoldy <hernanmdq@gmail.com>
2023-10-01 07:54:15 -0300

    agrego columnas para la DB del Rover

--- src/main/java/academy/atl/models/Rover.java
+++ src/main/java/academy/atl/models/Rover.java
@@ -1,11 +1,24 @@
 package academy.atl.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "rover")
 public class Rover {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "posX")
     private Integer x;
+
+    @Column(name = "posY")
     private Integer y;
+
+    @Column(name = "orientacion")
     private Orientacion orientacion;
 }

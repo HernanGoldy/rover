b5d719b9cdd86131233288eaf73813ca687c2a56
HernanGoldy <hernanmdq@gmail.com>
2023-10-01 08:51:14 -0300

    el controlador llama al servicio

---
+++ src/main/java/academy/atl/services/ObstaculoServiceImp.java
@@ -0,0 +1,16 @@
+package academy.atl.services;
+
+import academy.atl.dto.ObstaculoDto;
+import academy.atl.models.Obstaculo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ObstaculoServiceImp implements ObstaculoService {
+
+    @Override
+    public List<Obstaculo> mostrarObstaculos() {
+        return null;
+    }
+}
